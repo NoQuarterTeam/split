24c0042cbee4e98a4bca27cc795ce0abc9466db7
Jack Clackett <jclackett@gmail.com>
2019-02-16 00:28:16 +0100

    fix web api url

--- packages/web/src/config.tsx
+++ packages/web/src/config.tsx
@@ -2,5 +2,5 @@ export const env = process.env.NODE_ENV || "development"
 
 export const apiUrl =
   env === "production"
-    ? "https://nq-splitme.herokuapp.com/"
+    ? "https://nq-splitme.herokuapp.com/graphql"
     : "http://localhost:5000/graphql"

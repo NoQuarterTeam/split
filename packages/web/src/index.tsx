80580935df2a7b3b12de8455012326812da0ba6b
Jack Clackett <jclackett@gmail.com>
2019-02-16 12:14:25 +0100

    ad logrocket

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,13 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom"
 import ApolloClient from "apollo-boost"
 import { ApolloProvider } from "react-apollo-hooks"
+import LogRocket from "logrocket"
 
 import * as serviceWorker from "./serviceWorker"
 
 import Application from "./application"
 import { ThemeProvider, theme } from "./application/theme"
-import { apiUrl } from "./config"
+import { apiUrl, env } from "./config"
 import GlobalStyles from "./globalStyles"
+
+if (env === "production") {
+  LogRocket.init("yluxch/split")
+}
 
 const client = new ApolloClient({
   credentials: "include",

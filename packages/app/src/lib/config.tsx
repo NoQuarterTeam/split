03e90ffd1e8eaf322c0ab1384565269574d26e71
Jack Clackett <jclackett@gmail.com>
2019-05-12 11:37:49 +0200

    extract app from repo, upgrade to node 12

--- packages/app/src/lib/config.tsx
+++
@@ -1,8 +0,0 @@
-export const env = process.env.NODE_ENV || "development"
-
-export const production = env === "production"
-
-export const apiUrl =
-  env === "production"
-    ? "https://nq-split.herokuapp.com/graphql"
-    : "http://localhost:5000/graphql"
